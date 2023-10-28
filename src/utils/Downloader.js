5a1fb632c37cf3d5058da8357d76b61ab38bcd92
DarkerInk <rstonehouse66@gmail.com>
2023-10-28 14:50:25 -0500

    Update Downloader.js

--- src/utils/Downloader.js
+++ src/utils/Downloader.js
@@ -69,13 +69,7 @@ process.on('uncaughtException', async (error) => {
 });
 
 
-const downloadUrl = "https://raw.githubusercontent.com/Discord-Datamining/Discord-Datamining/master/current.js";
-// const downloadUrl = "https://raw.githubusercontent.com/Discord-Datamining/Discord-Datamining/0bfd0eebbf023108cddcb3ad7f2d45f722fe8b6a/current.js";
-// const downloadUrl = "https://raw.githubusercontent.com/Discord-Datamining/Discord-Datamining/dbc7d3a94fcd77ab33d7250809530a6eb1747853/current.js";
-// const downloadUrl = "https://raw.githubusercontent.com/Discord-Datamining/Discord-Datamining/528bc1eafcf356689e21134e70ce05ccb92f89d8/current.js"; // OCT 2022
-// const downloadUrl = "https://raw.githubusercontent.com/Discord-Datamining/Discord-Datamining/8a4e46c1b0bead45e8dd44b5aa133c18f1e31540/current.js"; // FEB 2021
-// const downloadUrl = "https://raw.githubusercontent.com/Discord-Datamining/Discord-Datamining/80a630881a97ec3e035be4f79973f917f968b597/current.js"; // FEB 2022
-// const downloadUrl = "https://raw.githubusercontent.com/Discord-Datamining/Discord-Datamining/e816b34f92b58fb1671cd81a7f133693e47ec49f/current.js" // May 2019
+const downloadUrl = "https://raw.githubusercontent.com/Darker-Ink/endpoint-downloader/master/currentRoutes.js";
 
 console.clear();
 
